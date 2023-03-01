47c6c282110f2142e301a6c4767c61272f521741
SsdHUN <santhasoma1999@gmail.com>
2023-03-01 16:57:43 +0100

    read the data from csv file

--- src/test/java/com/example/testautomation3/TestData.java
+++ src/test/java/com/example/testautomation3/TestData.java
@@ -6,10 +6,10 @@
 package com.example.testautomation3;
 
 public final class TestData {
-    public static final String USERNAME = "automation43";
-    public static final String FALSE_USERNAME = "randomusername";
-    public static final String PASSWORD = "CCAutoTest19.";
-    public static final String FALSE_PASSWORD = "randompassword";
+    public static final String USERNAME = Utils.readCsv("SuccessfulLoginData.csv").get(0)[0];
+    public static final String FALSE_USERNAME = Utils.readCsv("FalseLoginData.csv").get(0)[0];
+    public static final String PASSWORD = Utils.readCsv("SuccessfulLoginData.csv").get(0)[1];
+    public static final String FALSE_PASSWORD = Utils.readCsv("FalseLoginData.csv").get(0)[1];
     public static final String EXPECTED_LOGIN_ERROR_MESSAGE = "Sorry, your username and password are incorrect - please try again.";
     public static final String MTP_KEY = "MTP";
     public static final String TOUCAN_KEY = "TOUCAN";
